2591fe6eeffbd5857f48a27c03ccaf9a3bfd0887
Konstantin Kuritsyn <kuritsyn@yoomoney.ru>
2025-09-15 18:26:29 +0300

    #1 Динамическая память

--- lecture-01/lecture-01.cpp
+++ lecture-01/lecture-01.cpp
@@ -1,8 +1,17 @@
-﻿#include <iostream>
+﻿#include <iostream> // подключение в стиле С++ (без расширения h). Все загружается в пространство имен std
+
+// std - стандартное пространство имен
+
+
+
 
 int foo() { // int foo()
+
+	int y = 5;
+	int x = 6;
+
 	return 0;
-}
+} // y, x - уничтожаются
 
 
 namespace cpp2532m {
@@ -37,9 +46,19 @@ int foo(int x) {  // int foo_int(int x)
 // }
 
 
+using namespace std;
+
 int main()
 {
 	int k = foo(); // k = 0 вызывается функция из глобавльного пространства имен foo
+	// sizeof(int) байт занимает k
+
+	{
+		int k1 = 5;
+	} // k1 
+
+	int array[5]; // занимает 5 * sizeof(int) байт
+
 
 	int l = cpp2532m::foo(); // l=2 cpp2532m_foo
 
@@ -49,5 +68,64 @@ int main()
 	foo(3); // foo_int(3)
 
 	int n = cpp2532m::lecture01::foo(); // n = 1
+
+
+	std::cout // стандартный поток вывода
+		<<    // оператор вывода в поток
+		"k="  // левый операнд, результом является поток std::cout
+		<<
+		k
+		<<
+		std::endl; // объект завершения строки: перевод строки + команда для сброса буфера
+
+
+	// Стандартный поток ввода - std::cin
+
+	cout << "Enter a value for m: ";
+
+	cin >> //  оператор чтения из потока
+		m;
+
+	cout << endl << "m=" << m << endl;
+		
+
+	// Для размещения объектов в динамической памяти применяется оператор new/delete
+
+	int* kp = &k; // указатель kp на целое число равен адресу k
+
+	int k1 = *kp; // k1 = k, звездочка - операция разыменования
+	*kp = 666;    // k = 666, k1 = старое значение k
+
+
+	int* arrayp = array; // 
+	int* arrayp1 = &array[0]; // arrayp1=arrayp
+
+	*arrayp = 5;
+	// array[0] == 5
+	// *arrayp == 5 - потому что arrayp ссылается на нулевой элемент array
+
+	int first = array[0];
+	int first1 = arrayp[0]; // first1 = first
+
+
+	int* pa = new int; // 1) выдеяется sizeof(int) байт в динамической памяти, 2) возвращается указатель int* на эту область
+	*pa = 10; // записали значение в выделенную память
+
+	delete pa;
+
+	int* pb = new int(5); 
+	cout << "*pb=" << *pb << endl; // *pb=5
+	delete pb;
+
+	int* parr = new int[m]; // 1) выделяется память sizeof(int) * m, 2) возвращается указатель на нулевой элемент массива
+
+	for (int i = 0; i < m; ++i) {
+		cout << "parr[i]" << parr[i] << endl;
+	}
+
+	delete[] parr;
+
+
+
 }
 
