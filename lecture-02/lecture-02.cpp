a3fac640c2b7a0a98dc1645bd962219c0420a6d3
Konstantin Kuritsyn <kuritsyn@yoomoney.ru>
2025-11-10 18:37:24 +0300

    Лекция 4 Наследование

--- lecture-02/lecture-02.cpp
+++ lecture-02/lecture-02.cpp
@@ -144,69 +144,116 @@ public:
         return a;
     }
 
-    void print() {
-        cout << "a=" << a << endl;
+    void print() const {
+        cout << "a= " << a << endl;
     }
 
 };
 
 
+// B - наследник класса А (или дочерний класс для А)
+// A - базовый класс или родительский класс для B
+class B : public A {
+
+    // A base; -- все поля класса A
+
+    int b;
+
+public:
+    // В конструкторе класса B вызывается конструтор базового класса A
+    B(int a, int b) : A(a), b(b) {
+
+    }
+
+
+    // Метод A_getA_A* применимо и для объекта класса B
+    /*int getA() const {
+        return a;
+    }*/
+
+    int getB() const {
+        return b;
+    }
+
+    void print() const {
+        A::print();
+        cout << "b=" << b << endl;
+    }
+
+};
+
+void foo(A* aptr) {
+    if (aptr->getA() > 0) {
+        aptr->print();
+    }
+}
 
 int main()
 {
 
+    A a(1);
+
+    a.print(); // a=1
+
+    foo(&a);
+
+    B b(2, 3);
+    b.getA();
+    b.print(); // a=2 b=3
+
+    foo(&b);
     
 
-    Vector v1(5);
-    {
-        Vector v2; // конструктор по умолчанию
-        v2 = v1; // v2.data = v1.data (оператор присваивания по умолчанию копирует данные побайтово)
-    } // v2.~Vector() - уничтожается память, которой владеет v1
-    v1.print();
+    //Vector v1(5);
+    //{
+    //    Vector v2; // конструктор по умолчанию
+    //    v2 = v1; // v2.data = v1.data (оператор присваивания по умолчанию копирует данные побайтово)
+    //} // v2.~Vector() - уничтожается память, которой владеет v1
+    //v1.print();
 
-    std::cout << "==================" << std::endl;
+    //std::cout << "==================" << std::endl;
 
-    {
-        Vector v3 = v1; // конструктор копирования
-    }
-    v1.print();
+    //{
+    //    Vector v3 = v1; // конструктор копирования
+    //}
+    //v1.print();
 
-    std::cout << "==================" << std::endl;
+    //std::cout << "==================" << std::endl;
 
-    Vector v2 = foo(v1);
-    fooByPointer(&v1);
-    fooByReference(v1);
+    //Vector v2 = foo(v1);
+    //fooByPointer(&v1);
+    //fooByReference(v1);
 
-    std::cout << "==================" << std::endl;
-
-
-    Vector v3 = v1 + v2;
-    // Vector v3(v1.operator+(v2)) - перегрузка оператора + при помощи метода
-    // Vector v3(operator+(v1, v2)) - перегрузка оператор + при помощи функции
-
-    Vector v4 = 5 * v3;
-    // Vector v4(v3.operator*(int)) - перегрузка оператора * при помощи метода НЕ ПОДОЙДЕТ, т.к. левый оперантд - не Vector
-    // Vector v4(operator+(int, Vector)) - перегрузка оператор * при помощи функции
+    //std::cout << "==================" << std::endl;
 
 
-    v4 = v3 * 5;
+    //Vector v3 = v1 + v2;
+    //// Vector v3(v1.operator+(v2)) - перегрузка оператора + при помощи метода
+    //// Vector v3(operator+(v1, v2)) - перегрузка оператор + при помощи функции
+
+    //Vector v4 = 5 * v3;
+    //// Vector v4(v3.operator*(int)) - перегрузка оператора * при помощи метода НЕ ПОДОЙДЕТ, т.к. левый оперантд - не Vector
+    //// Vector v4(operator+(int, Vector)) - перегрузка оператор * при помощи функции
+
+
+    //v4 = v3 * 5;
 
   
-    v4[1] = 888; // v4.data[1] = 888
-    cout << "v4[1]=" << v4[1] << endl;
+    //v4[1] = 888; // v4.data[1] = 888
+    //cout << "v4[1]=" << v4[1] << endl;
 
-    const Vector& v5 = v4;
-    cout << "v5[1]=" << v5[1] << endl; // вызывается константная версия оператора []
-    // v5[1] = 666; - ошибка, нельзя писать в константу
+    //const Vector& v5 = v4;
+    //cout << "v5[1]=" << v5[1] << endl; // вызывается константная версия оператора []
+    //// v5[1] = 666; - ошибка, нельзя писать в константу
 
-    std::cout << "==================" << std::endl;
+    //std::cout << "==================" << std::endl;
 
-    Matrix m1(2, 3), m2(2, 3);
-    Matrix m3 = m1 + m2;
+    //Matrix m1(2, 3), m2(2, 3);
+    //Matrix m3 = m1 + m2;
 
-     m3[1][2] = 5; // (m3.operator[](1)).operator[](2) -- ссылка на int
+    // m3[1][2] = 5; // (m3.operator[](1)).operator[](2) -- ссылка на int
 
-    m3.operator[](1).operator[](2) = 5;
-    cout << "m3[1][2]=" << m3[1][2] << endl;
+    //m3.operator[](1).operator[](2) = 5;
+    //cout << "m3[1][2]=" << m3[1][2] << endl;
 
 } // Vector_~Vector_Vector*(&v1)
